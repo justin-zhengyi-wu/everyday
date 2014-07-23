6d5266f3e114ae2690c420c4976814322b1936df
Justin Wu <justin.zhengyi.wu@gmail.com>
2014-07-23 09:29:53 +0800

    Start to learn AngularJS.

---
+++ 2014-07-23/finance.js
@@ -0,0 +1,30 @@
+angular.module('finance', [])
+    .factory('currencyConverter', ['$http', function($http) {
+        var YAHOO_FINANCE_URL_PATTERN = '//query.yahooapis.com/v1/public/yql?q=select * from '+
+          'yahoo.finance.xchange where pair in ("PAIRS")&format=json&'+
+          'env=store://datatables.org/alltableswithkeys&callback=JSON_CALLBACK';
+        var currencies = ['USD', 'EUR', 'CNY'];
+        var usdToForeignRates = {};
+        var convert = function(amount, inCurr, outCurr) {
+            return amount * usdToForeignRates[outCurr] / usdToForeignRates[inCurr];
+        };
+        var refresh = function() {
+            var url = YAHOO_FINANCE_URL_PATTERN.replace('PAIRS', 'USD' + currencies.join('","USD'));
+            return $http.jsonp(url).success(function(data) {
+                var newUsdToForeignRates = {};
+                angular.forEach(data.query.results.rate, function(rate) {
+                    var currecy = rate.id.substring(3, 6);
+                    newUsdToForeignRates[currecy] = window.parseFloat(rate.Rate);
+                });
+                usdToForeignRates = newUsdToForeignRates;
+            });
+        };
+
+        refresh();
+
+        return {
+            currencies: currencies,
+            convert: convert,
+            refresh: refresh
+        };
+    }]);

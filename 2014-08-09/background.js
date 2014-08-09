7b68c26516ce590183b2910ed061e75c6e5a353f
Justin Wu <justin.zhengyi.wu@gmail.com>
2014-08-09 21:40:00 +0800

    Chrome App: Media manager

---
+++ 2014-08-09/background.js
@@ -0,0 +1,11 @@
+chrome.app.runtime.onLaunched.addListener(function() {
+    chrome.app.window.create('main.html', {
+        'id': 'main',
+        'bounds': {
+            'width': 800,
+            'height': 600
+        },
+        'resizable': true,
+        'frame': 'none'
+    });
+});
